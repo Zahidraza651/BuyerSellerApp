be6d04dc86e827e0c7df6eccfb1324eb8107648d
Arslan7t9 <51242765+Arslan7t9@users.noreply.github.com>
2022-08-05 11:30:45 +0500

    Revert "Waqas"

--- lib/notification/notification.dart
+++ lib/notification/notification.dart
@@ -1,144 +1,20 @@
-<<<<<<< HEAD
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localization.dart';
-import 'package:http/http.dart' as http;
-import '../constants.dart';
-import '../models/alert.dart';
-import '../models/user.dart';
-import '../widgets/Notificationwidget.dart';
-
-class NotificationScreen extends StatefulWidget {
-  final UserData userData;
-  const NotificationScreen({Key? key,required this.userData}) : super(key: key);
-=======
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
 
   @override
   State<NotificationScreen> createState() => _NotificationScreenState();
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
-<<<<<<< HEAD
-
-  int listItem = 0;
-  AlertModel? model;
-  bool isLoading = false;
-  Future getAlertData() async {
-    setState(() => isLoading = true);
-
-    var token = widget.userData.token;
-    final response = await http.get(
-      Uri.parse('$baseUrl/alerts'),
-      headers: {
-        //'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Bearer $token',
-      },
-    );
-    // print(response.body);
-    setState(() => isLoading = false);
-    if (response.statusCode == 200) {
-      setState((){
-        model = AlertModel.fromJson(jsonDecode(response.body));
-        listItem = model!.data.length;
-      });
-
-
-    } else {
-      _showMsg(
-          'Server Error ${response.statusCode}',
-          const Icon(
-            Icons.close,
-            color: Colors.red
-          ));
-    }
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getAlertData();
-  }
-
-
-
-=======
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             body: Padding(
       padding: const EdgeInsets.all(18.0),
-<<<<<<< HEAD
-            child: Column(
-        children: [
-              const SizedBox(
-                height: 20.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    AppLocalizations.of(context)!.alerts, //'Alerts',
-                    style:
-                        const TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold),
-                  ),
-                  InkWell(
-                    child: Text(
-                      AppLocalizations.of(context)!.clearAll, //'Clear All',
-                      style: const TextStyle(color: Colors.blue),
-                    ),
-                    onTap: () {
-                      //TODO for clear notifications
-                    },
-                  )
-                ],
-              ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: listItem,
-              shrinkWrap: true,
-              itemBuilder: (context,index){
-                return NotificationList(title: model!.data[index].title!,created_at: model!.data[index].createdAt!,
-                description: model!.data[index].description!,status: model!.data[index].status!,);
-              },
-              // children: [
-              //
-
-              // ],
-            ),
-          ),
-        ],
-      ),
-    )));
-  }
-  _showMsg(String msg, Icon icon) {
-    final snackBar = SnackBar(
-      content: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Text(
-              msg,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          icon
-        ],
-      ),
-      duration: const Duration(seconds: 2),
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-  }
-=======
       child: SingleChildScrollView(
         child: Column(
           children: [
@@ -302,5 +178,4 @@ class _NotificationScreenState extends State<NotificationScreen> {
       ),
     )));
   }
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
 }
--- lib/widgets/statusWidget.dart
+++ lib/widgets/statusWidget.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
-<<<<<<< HEAD
-=======
 import 'package:seller_side/models/user.dart';
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
 
 import '../home_screen/invoice.dart';
 
@@ -12,22 +9,15 @@ class Statuswidget extends StatefulWidget {
   final String itemtype;
   final String price;
   final String detail;
-<<<<<<< HEAD
-=======
   final UserData userData;
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
 
   const Statuswidget(
       {Key? key,
       required this.detail,
       required this.itemtype,
       required this.price,
-<<<<<<< HEAD
-      required this.status})
-=======
       required this.status,
       required this.userData})
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
       : super(key: key);
 
   @override
@@ -53,11 +43,6 @@ class _StatuswidgetState extends State<Statuswidget> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
-<<<<<<< HEAD
-                    padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                    decoration: BoxDecoration(
-                        color: Color(0xFFF2CC59), borderRadius: BorderRadius.circular(20.0)),
-=======
                     padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
                     decoration: BoxDecoration(
                         color: widget.status == 'Pending'
@@ -66,7 +51,6 @@ class _StatuswidgetState extends State<Statuswidget> {
                                 ? const Color(0xffFF3B3B)
                                 : const Color(0xff2CC91F),
                         borderRadius: BorderRadius.circular(20.0)),
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
                     child: Center(
                       child: Text(
                         widget.status, //'Unread',
@@ -80,11 +64,7 @@ class _StatuswidgetState extends State<Statuswidget> {
               children: [
                 Expanded(
                     child: Padding(
-<<<<<<< HEAD
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-=======
                   padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
                   child: Text(
                     AppLocalizations.of(context)!.itemtype, //'Alert Title',
                     style: const TextStyle(
@@ -98,11 +78,7 @@ class _StatuswidgetState extends State<Statuswidget> {
               children: [
                 Expanded(
                     child: Padding(
-<<<<<<< HEAD
-                  padding: EdgeInsets.fromLTRB(0, 7, 0, 0),
-=======
                   padding: const EdgeInsets.fromLTRB(0, 7, 0, 0),
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
                   child: Text(
                     widget.itemtype, //'Alert Title',
                     style: const TextStyle(
@@ -116,15 +92,9 @@ class _StatuswidgetState extends State<Statuswidget> {
               children: [
                 Expanded(
                     child: Padding(
-<<<<<<< HEAD
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: Text(
-                    AppLocalizations.of(context)!.price, //'Alert Title',
-=======
                   padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
                   child: Text(
                     AppLocalizations.of(context)!.price,
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
                     style: const TextStyle(
                         color: Color(0xFF969696), fontSize: 15.0, fontWeight: FontWeight.bold),
                   ),
@@ -136,15 +106,9 @@ class _StatuswidgetState extends State<Statuswidget> {
               children: [
                 Expanded(
                     child: Padding(
-<<<<<<< HEAD
-                  padding: EdgeInsets.fromLTRB(0, 7, 0, 0),
-                  child: Text(
-                    "\$${widget.price}", //'Alert Title',
-=======
                   padding: const EdgeInsets.fromLTRB(0, 7, 0, 0),
                   child: Text(
                     "\$${widget.price}",
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
                     style: const TextStyle(
                         color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.bold),
                   ),
@@ -156,8 +120,6 @@ class _StatuswidgetState extends State<Statuswidget> {
               children: [
                 Expanded(
                     child: Padding(
-<<<<<<< HEAD
-=======
                   padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
                   child: Text(
                     AppLocalizations.of(context)!.details,
@@ -172,17 +134,12 @@ class _StatuswidgetState extends State<Statuswidget> {
               children: [
                 Expanded(
                     child: Padding(
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
                   padding: const EdgeInsets.all(5.0),
                   child: Text(
                     widget.detail,
                     //overflow: TextOverflow.ellipsis,
-<<<<<<< HEAD
-                    style: TextStyle(color: Colors.black, fontSize: 12.0, fontWeight: FontWeight.normal),
-=======
                     style: const TextStyle(
                         color: Colors.black, fontSize: 12.0, fontWeight: FontWeight.normal),
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
                   ),
                 ))
               ],
@@ -191,16 +148,12 @@ class _StatuswidgetState extends State<Statuswidget> {
         ),
       ),
       onTap: () {
-<<<<<<< HEAD
-        // Navigator.push(context, MaterialPageRoute(builder: (context) =>  Invoice()));
-=======
         Navigator.push(
             context,
             MaterialPageRoute(
                 builder: (context) => Invoice(
                       userdata: widget.userData,
                     )));
->>>>>>> acfb8e063a2dd0639e4b385f4beea358d29ff1b7
       },
     );
   }
